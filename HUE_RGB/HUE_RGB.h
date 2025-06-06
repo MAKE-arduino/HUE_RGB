e65ed3664bec9d846a456e5bde11574b09ae9d8f
Make - arduino, diy <alexey.morozov.2023@mail.ru>
2025-06-06 11:20:21 +0300

    HUE_RGB_V2.h
    Добавлено цветовое пространство RGB, как пользоваться смотри в README.

--- HUE_RGB/HUE_RGB.h
+++ HUE_RGB/HUE_RGB.h
@@ -13,34 +13,43 @@ class LED{
     pinMode(_Rpin, OUTPUT);
         }  
     
-    void write(byte color){
-    _color = color;
-    if(_color < 85){
-      _Rclr = 85 - _color;
-      _Gclr = _color;
-      _Bclr = 0;
-    }
-    else if(_color < 170){
-      _Rclr = 0;
-      _Gclr = 170 - _color;
-      _Bclr = _color - 85;
-    }
-    else{
-      _Rclr = _color - 170;
-      _Gclr = 0;
-      _Bclr = 255 - _color;
-    }
-  analogWrite(_Rpin, _Rclr);
-  analogWrite(_Gpin, _Gclr);
-  analogWrite(_Bpin, _Bclr);
+    void HUE_write(byte color){ // пространство RGB
+      _HUEcolor = color;
+      if(_color < 85){  // если значение цвета меньше 85 то, установить такие цвета
+        _Rclr = 85 - _color;
+        _Gclr = _color;
+        _Bclr = 0;
+      }
+      else if(_color < 170){  // если значение цвета меньше 170 то, установить такие цвета
+        _Rclr = 0;
+        _Gclr = 170 - _color;
+        _Bclr = _color - 85;
+      }
+      else{
+        _Rclr = _color - 170; // иначе, установить такие цвета
+        _Gclr = 0;
+        _Bclr = 255 - _color;
+      }
+      analogWrite(_Rpin, _Rclr);
+      analogWrite(_Gpin, _Gclr);
+      analogWrite(_Bpin, _Bclr);
   }
-  private:
-    byte _Rpin;
-    byte _Gpin;
-    byte _Bpin;
-    byte _color;
-    bool _flag;
-    byte _Rclr, _Gclr, _Bclr;
+
+    void RGB_write(byte Rclr1, byte Gclr1, byte Bclr1){ // пространство RGB
+      _Rclr1 = Rclr1; 
+      _Gclr1 = Gclr1;
+      _Bclr1 = Bclr1;
+      analogWrite(_Rpin, _Rclr1); // уровень красново
+      analogWrite(_Gpin, _Gclr1); // уровень зелёного
+      analogWrite(_Bpin, _Bclr1); // уровень синего
+    }
+
+  private: 
+    // приватные переменные:
+    byte _Rpin, _Gpin, _Bpin; // пины светодиода 
+    byte _HUEcolor; // переменная значения цвета на вход HUE (0 - 255)
+    byte _Rclr1, _Gclr1, _Bclr1; // переменная цвета RGB
+    byte _Rclr, _Gclr, _Bclr; // переменная значения цвета на выходе по цветам (HUE)
 };
 
 #endif
